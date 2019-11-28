1b50ad8fee271f12e0b889225850c1b359e9172c
Jamal1463 <mohd.jama412@gmail.com>
2019-11-28 15:27:48 +0530

    first commit dbutils.java

---
+++ dbutils.java
@@ -0,0 +1,11 @@
+
+Stylish Star Allu Arjun who is also a fabulous dancer
+is introducing variety dance moves in his 
+upcoming film Ala Vaikunthapuramulo.
+
+His 'Half Coat Step' From Ramulo Ramula song
+is a major hit on YouTube. 
+TikTokers are having a ball recreating that step from the day it was launched.
+
+Encouraged by the response, makers of Ala Vaikunthapuramulo
+revealed the 'Hook Move' from OMG Daddy song.
