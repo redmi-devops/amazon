47b387727c8c12d072abda96d887e60ee2553ba3
Jamal1463 <mohd.jama412@gmail.com>
2019-11-28 15:44:16 +0530

    new commit to strings.java

---
+++ strings.java
@@ -0,0 +1,14 @@
+
+Nikhil's long-awaited film Arjun Suravaram is finally releasing on November 29th.
+
+The movie unit is actively promoting the investigative thriller and the male lead,
+
+Nikhil revealed some interesting stuff about 'Arjun Suravaram'.
+
+"Arjun Suravaram was postponed multiple times. At one stage,
+
+I had many sleepless nights, fearing this film might never hit the big screens.
+
+It took a toll on me both mentally and physically," he said.
+
+Nikhil said Chiranjeevi came out in support of the film when the movie unit needed it the most.
